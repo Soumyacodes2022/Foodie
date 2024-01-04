5e01d9078d7610421f3a6854ab7cc76810f4129f
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-01-04 11:44:16 +0530

    Landing Page UI

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 import "../App.css"
+import Footer from '../Components/Footer'
 
 const Main = () => {
   return (
     <div>
         <nav><Navbar/></nav>
         <Outlet/>
-        {/* <footer>Footer</footer> */}
+        <footer><Footer/></footer>
     </div>
   )
 }
