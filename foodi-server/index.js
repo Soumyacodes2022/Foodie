02e1fc7910ab033de8b3fe7a085d3add36dc4956
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-01-09 14:34:57 +0530

    backend created

--- foodi-server/index.js
+++ foodi-server/index.js
@@ -1,1 +1,11 @@
-sfdjaojfiadf
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.get('/', (req,res)=>{
+    res.send("Hello Developer!");
+})
+
+app.listen(port,()=>{
+    console.log(`Listening at por ${port}`);
+})
