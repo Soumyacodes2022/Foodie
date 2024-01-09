df201faacd5b9aaad40053aa1c189def657e41ff
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-01-09 15:20:59 +0530

    cors update

--- foodi-server/index.js
+++ foodi-server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
+//middleware
+app.use(cors())
+app.use(express.json())
+
+//mongo atlas
+//username: flash_34
+//password: c9vM75WbkHejQkHS
+
 app.get('/', (req,res)=>{
     res.send("Hello Developer!");
 })
