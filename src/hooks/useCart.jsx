75df215a17d86113ae1ac4a49dc13b036d662147
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-01-11 20:10:30 +0530

    tanstack_added

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,12 @@
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthProvider"
+
+
+const useCart = () => {
+    const {user} = useContext(AuthContext)
+  return (
+    <div>useCart</div>
+  )
+}
+
+export default useCart
