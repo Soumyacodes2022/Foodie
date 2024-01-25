4900bb131369942446e663c3315eacdc8f99edc2
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-01-25 14:49:08 +0530

    menuItems_API_Added

--- api/controllers/menuController.js
+++ api/controllers/menuController.js
@@ -9,6 +9,21 @@ const getAllMenuItems = async(req,res)=>{
     }
     
 }
+
+const postNewMenuItem = async(req,res)=> {
+    const newItem = req.body;
+    try {
+
+        const result = await Menu.create(newItem);
+        res.status(200).json(result);
+        
+    } catch (error) {
+    
+        res.status(500).json({message: error.message})
+        
+    }
+}
 module.exports={
-    getAllMenuItems
+    getAllMenuItems,
+    postNewMenuItem
 }
--- api/router/menuRouter.js
+++ api/router/menuRouter.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const menuController = require('../controllers/menuController')
 //get all menu items
 router.get('/', menuController.getAllMenuItems)
-
+router.post('/',menuController.postNewMenuItem)
 module.exports = router
