de8ef88c500c6bc963bd5c66635027c48aec57eb
Soumyacodes2022 <kalpanaganguly.behala94@gmail.com>
2024-01-26 22:29:18 +0530

    Manage Menu Item API Added

--- api/controllers/menuController.js
+++ api/controllers/menuController.js
@@ -2,7 +2,7 @@ const Menu = require('../model/Menu');
 
 const getAllMenuItems = async(req,res)=>{
     try {
-        const menus = await Menu.find({});
+        const menus = await Menu.find({}).sort({createdAt: -1});
         res.status(200).json(menus)
     } catch (error) {
         res.status(500).json({message: error.message})
@@ -23,7 +23,50 @@ const postNewMenuItem = async(req,res)=> {
         
     }
 }
+
+const deleteMenuItem = async(req,res) => {
+    const menuId = req.params.id;
+    try {
+        const deleteItem = await Menu.findByIdAndDelete(menuId);
+        if(!deleteItem){
+            return res.status(400).json({message:"MenuItem Not Found"});
+        }
+        res.status(200).json(deleteItem)
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
+
+const getSingleMenuItem = async(req,res) => {
+    const menuId = req.params.id;
+    try {
+        const getItem = await Menu.findById(menuId);
+        res.status(200).json(getItem)
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+const updateMenuItem = async(req,res) => {
+    const menuId = req.params.id;
+    const {_id,name,recipe,image,category,price} = req.body;
+    try {
+        const updatedMenu = await Menu.findByIdAndUpdate(menuId , {_id,name,recipe,image,category,price} , {
+            new: true,
+            runValidators:true
+        })
+        if(!updatedMenu) {
+            return res.status(404).json({message:"Menu Item not found."})
+        }
+        res.status(200).json(updatedMenu)
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
 module.exports={
     getAllMenuItems,
-    postNewMenuItem
+    postNewMenuItem,
+    deleteMenuItem,
+    getSingleMenuItem,
+    updateMenuItem
 }
--- api/router/menuRouter.js
+++ api/router/menuRouter.js
@@ -4,4 +4,7 @@ const menuController = require('../controllers/menuController')
 //get all menu items
 router.get('/', menuController.getAllMenuItems)
 router.post('/',menuController.postNewMenuItem)
+router.delete('/:id',menuController.deleteMenuItem)
+router.get('/:id',menuController.getSingleMenuItem)
+router.patch('/:id',menuController.updateMenuItem)
 module.exports = router
